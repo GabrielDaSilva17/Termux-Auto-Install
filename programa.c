4e2e0e1cf678ef039e76e0aca73a326803a0d41b
Gabriel <112807883+GabrielDaSilva17@users.noreply.github.com>
2026-02-03 16:44:32 +0100

    Refactor window creation and color settings

--- programa.c
+++ programa.c
@@ -1,19 +1,27 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include "config.h" // Importa suas configurações personalizadas
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Janela do Gabriel", 
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 480, SDL_WINDOW_SHOWN);
+
+    SDL_Window* window = SDL_CreateWindow(
+        TITULO, 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        LARGURA, 
+        ALTURA, 
+        SDL_WINDOW_SHOWN
+    );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     
-    // Pinta o fundo de azul
-    SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
+    // Usa as cores definidas no config.h
+    SDL_SetRenderDrawColor(renderer, COR_R, COR_G, COR_B, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(5000); // Fica aberta por 5 segundos
+    SDL_Delay(5000); 
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
